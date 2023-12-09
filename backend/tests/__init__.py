86a548d66a1a4b29a1b91fadf87b28f0626558d3
BuildTools <unconfigured@null.spigotmc.org>
2023-12-08 19:17:23 -0700

    change

--- backend/tests/__init__.py
+++ backend/tests/__init__.py
@@ -1,1 +1,1 @@
-
+import backend.app
--- backend/tests/test_amqp_handler.py
+++ backend/tests/test_amqp_handler.py
@@ -1,1 +1,12 @@
 # Unit tests for the AMQP handler
+
+import sys
+sys.path.append('C:\\Users\\jacob\\Desktop\\github\\smarthome')
+sys.path.append('C:\\Users\\jacob\\Desktop\\github\\smarthome\\backend')
+sys.path.append('C:\\Users\\jacob\\Desktop\\github\\smarthome\\backend\\app')
+
+print(sys.path)
+
+import unittest
+from unittest.mock import patch
+import backend.app
