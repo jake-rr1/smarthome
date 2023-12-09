8b62951439555001be4b052fed6242fdcc36237a
jake-rr1 <jacobroach99@gmail.com>
2023-12-08 23:33:41 -0700

    change-alac

--- backend/tests/test_amqp_handler.py
+++ backend/tests/test_amqp_handler.py
@@ -1,10 +1,12 @@
 # Unit tests for the AMQP handler
 
+
 import sys
-sys.path.append('C:\\Users\\jacob\\Desktop\\github\\smarthome')
 
 print(sys.path)
 
 import unittest
 from unittest.mock import patch
 import backend.app
+
+
